35945fd06b61cc7d981847c9e6990f6b51062522
Sagar Prasad Mahto <sagarlovev143@gmail.com>
2026-01-12 10:47:48 +0530

    Update portfolio and features

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",   // repo name
+  base: "/smart-tast-manager/",   // repo name
 })
